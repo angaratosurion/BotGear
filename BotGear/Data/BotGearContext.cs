f8bb497aa5812f6bf766b2b1bf3ce6fed89e4c6e
angaratosurion <angarato.surion@gmail.com>
2017-04-10 03:16:14 +0300

    aa
    starting adding  user models for databases :P

---
+++ BotGear/Data/BotGearContext.cs
@@ -0,0 +1,22 @@
+﻿using BotGear.Data.Models;
+using System.Data.Entity;
+
+namespace BotGear.Data
+{
+    public class BotGearContext :DbContext
+    {
+        public BotGearContext()
+            : base("DefaultConnection")
+        {
+            //this.Configuration.AutoDetectChangesEnabled = true;
+            //this.Configuration.LazyLoadingEnabled = true;
+            //this.Configuration.ValidateOnSaveEnabled = false;
+        }
+        public static BotGearContext Create()
+        {
+            return new BotGearContext();
+        }
+
+       public IDbSet<BotGearUser> User { get; set; }
+    }
+}
---
+++ BotGear/Data/Models/BotGearUser.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotGear.Data.Models
+{
+    public class BotGearUser : ISnowflakeEntity//, IMentionable//, IPresence
+    {
+        /// <summary> Gets the id of this user's avatar. </summary>
+        public string AvatarId { get; set; }
+        /// <summary> Gets the url to this user's avatar. </summary>
+        public string GetAvatarUrl { get; set; }
+        /// <summary> Gets the per-username unique id for this user. </summary>
+        public string Discriminator { get; set; }
+        /// <summary> Gets the per-username unique id for this user. </summary>
+        public ushort DiscriminatorValue { get; set; }
+        /// <summary> Returns true if this user is a bot user. </summary>
+        public bool IsBot { get; set; }
+        /// <summary> Returns true if this user is a webhook user. </summary>
+        public bool IsWebhook { get; set; }
+        /// <summary> Gets the username for this user. </summary>
+        ///  [Required]
+        public string Username { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+        [Required]
+        public ulong Id { get; set; }
+        [Required]
+        public int uid { get; set; }
+        public DateTime Birthday{ get; set; }
+    }
+}
