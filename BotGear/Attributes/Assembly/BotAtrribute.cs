aac8dbb3264a2919fc34be49ad950345a319eb7d
angaratosurion <angarato.surion@gmail.com>
2017-05-29 16:55:03 +0300

    z
    added a new attibut in order to make it lunch more bot's

---
+++ BotGear/Attributes/Assembly/BotAtrribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotGear.Attributes.Assembly
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    
+    public class BotAtrribute :  Attribute
+    {
+        public BotAtrribute(Boolean arg)
+        {
+            isBot = arg;
+        }
+        public Boolean isBot { get; set; }
+    }
+}
